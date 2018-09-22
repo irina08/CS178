00dd82979159f6db24d3d8b7d234a5a3e11804c3
irina08 <irina_ger@yahoo.com>
2018-09-22 15:53:15 -0700

    Add all projects

---
+++ Ant_HelloWorld_Java/HelloWorld.java
@@ -0,0 +1,10 @@
+// Student CS178 Irina Golovko.
+
+public class HelloWorld
+{
+	public static void main (String ... args)
+	{
+		System.out.println("Hello World!");
+	}
+}
+
---
+++ Make_HelloWorld/HelloWorld.cpp
@@ -0,0 +1,11 @@
+// NAME: Student CS178 Irina Golovko
+// FILE: HelloWorld.cpp
+// DATE: August 28, 2018
+// Desc: write a Makefile to build the
+// HelloWorld program in c++.
+#include <iostream>
+
+    int main(){
+    std::cout << "Hello, World!\n";
+    return 0;
+    }  
